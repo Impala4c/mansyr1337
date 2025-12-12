c957308a316e53723148f74055d366a96450e05a
Impala4c <mansuradan9@gmail.com>
2025-12-12 22:14:45 +0500

    task 8 updated

--- AP/task 8.py
+++ AP/task 8.py
@@ -1,10 +1,6 @@
 w = str(input("Enter a word: "))
 n = int(input("Enter a number: "))
-num = n 
-init = 0
-while num > 0:
-    for n in range(len(w)):
-        print(w[init])
-    init += 1
-    num-=1
+for char in w:
+    print(char * n)
+
     
