92fc77e065fe726d90f1267da6df27ae810b30ba
Impala4c <mansuradan9@gmail.com>
2025-12-20 23:39:57 +0500

    task 1 week 2

---
+++ AP/Week 2/task 1.py
@@ -0,0 +1,8 @@
+c = 0 
+a = ">>-->" 
+b = "<--<<" 
+st = str(input("Enter the string: ")) 
+for i in range(len(st)-4): 
+    if st[i:i+5] == a or st[i:i+5] == b: 
+        c += 1 
+print("Number of arrows found:", c)
