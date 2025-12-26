c0dba54ac0b18855b7f74d4be7bf54cf9b8abe42
Impala4c <mansuradan9@gmail.com>
2025-12-26 12:28:44 +0500

    task 1.1

---
+++ AP/Week 3/task 1.1.py
@@ -0,0 +1,8 @@
+pi = 3.14
+r = int(input("Radius for calcutaing area of circle:"))
+cir = pi * r * r 
+a = int(input("The side of square:"))
+squ = a * a
+z = int(input("Base of right triangle:"))
+y = int(input("Height of right triangle"))
+tri = 0.5*y*z
