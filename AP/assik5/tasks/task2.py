1758493c186a83c77fe05816240d892b1ba721be
Impala4c <mansuradan9@gmail.com>
2026-01-10 22:10:09 +0500

    json file example + working task2

--- AP/assik5/tasks/task2.py
+++ AP/assik5/tasks/task2.py
@@ -1,11 +1,14 @@
 import json
+import os
 
-with open("students.json", "r", encoding="utf-8") as f:
+BASE_DIR = os.path.dirname(__file__)
+json_path = os.path.join(BASE_DIR, "students.json")
+
+with open(json_path, "r", encoding="utf-8") as f:
     students = json.load(f)
 
 for student in students:
     grades = student["grades"]
     student["average_grade"] = sum(grades) / len(grades)
 
-with open("students_with_average.json", "w", encoding="utf-8") as f:
-    json.dump(students, f, indent=4)
+
